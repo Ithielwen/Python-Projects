ba12b33d096bb267924b4e66c90fd62e3f4a0cfb
Emma Hungrige <103974683+Ithielwen@users.noreply.github.com>
2022-05-10 21:49:29 -0700

    Add files via upload

---
+++ week4-hilo-methuselah/HiLo.py
@@ -0,0 +1,76 @@
+import random
+from Card import Card
+from Deck import Deck
+from Player import Player
+
+#Variables
+
+carddeck = Deck()
+carddeck.shuffle()
+
+human = Player()
+
+#Gameplay Functions
+
+def playagain():
+    """Function to give user option to play the game again or end it
+        
+        Parameters: None
+        Return: None
+    """
+    answer = input("Play again? Y or N \n").upper().strip()
+
+    if answer == "Y" and human.points > 0:
+
+        print("Okay!")
+        playgame()
+
+    elif answer == "N" or human.points <= 0:
+        print("Thanks for playing!")
+        endgame()
+
+    else:
+        print("INVALID ANSWER! GAME OVER!")
+        endgame()
+
+def endgame():
+    """Function to end the program
+        
+        Parameters: None
+        Return: None
+    """
+
+    print("GAME OVER! THANK YOU FOR PLAYING!")
+    quit()
+
+def playgame():
+    """Function for gameplay 
+    
+        Parameters: None
+        Return: None
+    """
+    
+    card = carddeck.drawCard()
+
+    nextCard = carddeck.drawCard()
+
+    points = human.points
+
+    print(f"The current card is: {card}")
+    
+    guess = input("Guess H for high or L for low: \n").upper().strip()
+    correct = human.guessCard(guess, card, nextCard)
+
+    print(f"The next card was...{nextCard}")
+    human.points = human.updateScore(correct, points)
+
+    playagain()
+
+def main():
+    
+    print("Welcome to HiLo! Let's get ready to rumble!")
+    playgame()
+
+if __name__ == "__main__":
+    main()
+
